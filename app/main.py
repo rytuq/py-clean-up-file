367023dab88e3b6f349082180e96eafad39e1595
inc <inc@email.com>
2026-09-15 01:43:23 +0300

    task resolved

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename: str = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: object | None,
+    ) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
